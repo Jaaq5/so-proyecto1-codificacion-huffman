f8a00a1bf1edb589cee5bf6e32475e62742fff58
Alex Artavia <joseaaq5@gmail.com>
2025-04-28 22:46:02 -0600

    Agregar comentarios

---
+++ archivos_recuperados_s/README.md
@@ -0,0 +1,18 @@
+# so-proyecto1-codificacion-huffman
+Repositorio para el proyecto 01 de so, codificacion de Huffman
+
+# Requisitos: gcc
+
+sudo dnf install gcc
+
+gcc --version
+
+# Como ejecutar el programa desde terminal
+git clone https://github.com/Jaaq5/so-proyecto1-codificacion-huffman
+
+cd so-proyecto1-codificacion-huffman
+
+make
+
+./proyecto1
+
--- main.c
+++ main.c
@@ -7,17 +7,16 @@ void EjecutarHuffmanSerial(int argc, char *argv[]);
 void EjecutarHuffmanPthread(int argc, char *argv[]);
 void EjecutarHuffmanFork(int argc, char *argv[]);
 
-
 int main() {
     int opcion;
-    char dir_txt[1024];  // Para almacenar el directorio de entrada o archivo
-    char archivo_salida[1024];  // Para almacenar el archivo de salida o directorio destino
-    char *argv[4];  // Usaremos un arreglo para pasar los parámetros a las funciones
+    char dir_txt[1024];          // Para almacenar el directorio de entrada o archivo
+    char archivo_salida[1024];   // Para almacenar el archivo de salida o directorio destino
+    char *argv[4];               // Arreglo para pasar los parámetros a las funciones
 
-    // Solicitar al usuario que elija entre Huffman Serial o Pthread
-    printf("Proyecto 1 - Comparación Huffman Serial vs Paralelo\n");
+    // Imprimir el mensaje de bienvenida y opciones al usuario
+    printf("Proyecto 1 - Codificacion de Huffman\n");
 
-    // Ciclo para asegurar que el usuario elija 1 o 2
+    // Ciclo para asegurar que el usuario elija 1, 2 o 3
     do {
         printf("Seleccione una opción:\n");
         printf("1. Ejecutar Huffman Serial\n");
@@ -26,70 +25,93 @@ int main() {
         printf("Ingrese su opción (1 o 3): ");
         scanf("%d", &opcion);
 
+        // Verificar que la opción sea válida
         if (opcion != 1 && opcion != 2 && opcion != 3) {
-            printf("\nOpción no válida. Por favor, elija 1 o 3.\n");
+            printf("\nOpción no válida. Por favor, elija 1, 2 o 3.\n");
         }
     } while (opcion != 1 && opcion != 2 && opcion != 3);
 
-    // Solicitar el directorio o archivo de entrada y archivo de salida/destino
+    // Solicitar el directorio o archivo de entrada y el archivo de salida o directorio destino
     if (opcion == 1) {
+        // Mensaje para indicar que se ejecutará la versión serial
         printf("\nEjecutando versión Serial...\n");
-        printf("Ingrese el directorio de archivos de texto para comprimir/descomprimir: ");
-        scanf("%s", dir_txt);
-        printf("Ingrese el archivo de salida para comprimir o el directorio destino para descomprimir: ");
-        scanf("%s", archivo_salida);
+        printf("Para comprimir ingrese: <directorio_con_txt> (Ej:Top30Gutenberg)\n");
+        printf("Para descomprimir ingrese: <archivo_comprimido_s.bin>\n");
+        scanf("%s", dir_txt);  // Leer el directorio de archivos
+        printf("Para comprimir ingrese: <archivo_comprimido_s.bin>\n");
+        printf("Para descomprimir ingrese: <archivos_recuperados_s>\n");
+        scanf("%s", archivo_salida);  // Leer el archivo de salida
 
         // Preparar los argumentos para la función EjecutarHuffmanSerial
-        argv[0] = "./huffman";  // El nombre del ejecutable
-        argv[1] = (char *)malloc(2); // Para el flag (-c o -d)
+        argv[0] = "./huffman";  // El nombre del ejecutable, se ignora cuando se juntaron los 3 en este main
+        argv[1] = (char *)malloc(2); // Asignar espacio para el flag (-c o -d)
+
+        // Determinar si se debe comprimir o descomprimir según la extensión del archivo de salida
         if (archivo_salida[strlen(archivo_salida)-4] == '.') {
             argv[1] = "-c";  // Comprimir
         } else {
             argv[1] = "-d";  // Descomprimir
         }
-        argv[2] = dir_txt;
-        argv[3] = archivo_salida;
 
-        EjecutarHuffmanSerial(4, argv); // Llamar la función
+        argv[2] = dir_txt;         // Directorio de entrada
+        argv[3] = archivo_salida;  // Archivo de salida
+
+        // Llamar la función correspondiente para ejecutar Huffman en modo serial
+        EjecutarHuffmanSerial(4, argv);
+
     } else if (opcion == 2) {
+        // Mensaje para indicar que se ejecutará la versión con Pthreads
         printf("\nEjecutando versión Pthread...\n");
-        printf("Ingrese el directorio de archivos de texto para comprimir/descomprimir: ");
-        scanf("%s", dir_txt);
-        printf("Ingrese el archivo de salida para comprimir o el directorio destino para descomprimir: ");
-        scanf("%s", archivo_salida);
+        printf("Para comprimir ingrese: <directorio_con_txt> (Ej:Top30Gutenberg)\n");
+        printf("Para descomprimir ingrese: <archivo_comprimido_p.bin>\n");
+        scanf("%s", dir_txt);  // Leer el directorio de archivos
+        printf("Para comprimir ingrese: <archivo_comprimido_p.bin>\n");
+        printf("Para descomprimir ingrese: archivos_recuperados_p\n");
+        scanf("%s", archivo_salida);  // Leer el archivo de salida
 
         // Preparar los argumentos para la función EjecutarHuffmanPthread
-        argv[0] = "./huffman";  // El nombre del ejecutable
-        argv[1] = (char *)malloc(2); // Para el flag (-c o -d)
+        argv[0] = "./huffman";  // El nombre del ejecutable, se ignora cuando se juntaron los 3 en este main
+        argv[1] = (char *)malloc(2); // Asignar espacio para el flag (-c o -d)
+
+        // Determinar si se debe comprimir o descomprimir según la extensión del archivo de salida
         if (archivo_salida[strlen(archivo_salida)-4] == '.') {
             argv[1] = "-c";  // Comprimir
         } else {
             argv[1] = "-d";  // Descomprimir
         }
-        argv[2] = dir_txt;
-        argv[3] = archivo_salida;
 
-        EjecutarHuffmanPthread(4, argv); // Llamar la función
+        argv[2] = dir_txt;  // Directorio de entrada
+        argv[3] = archivo_salida;  // Archivo de salida
+
+        // Llamar la función correspondiente para ejecutar Huffman usando Pthreads
+        EjecutarHuffmanPthread(4, argv);
 
     } else if (opcion == 3) {
+        // Mensaje para indicar que se ejecutará la versión con Fork
         printf("\nEjecutando versión Fork...\n");
-        printf("Ingrese el directorio de archivos de texto para comprimir/descomprimir: ");
-        scanf("%s", dir_txt);
-        printf("Ingrese el archivo de salida para comprimir o el directorio destino para descomprimir: ");
-        scanf("%s", archivo_salida);
+        printf("Para comprimir ingrese: <directorio_con_txt> (Ej:Top30Gutenberg)\n");
+        printf("Para descomprimir ingrese: <archivo_comprimido_f.bin>\n");
+        scanf("%s", dir_txt);  // Leer el directorio de archivos
+        printf("Para comprimir ingrese: <archivo_comprimido_f.bin>\n");
+        printf("Para descomprimir ingrese: <archivos_recuperados_f>\n");
+        scanf("%s", archivo_salida);  // Leer el archivo de salida
 
-        // Preparar los argumentos para la función EjecutarHuffmanPthread
-        argv[0] = "./huffman";  // El nombre del ejecutable
-        argv[1] = (char *)malloc(2); // Para el flag (-c o -d)
+        // Preparar los argumentos para la función EjecutarHuffmanFork
+        argv[0] = "./huffman";  // El nombre del ejecutable, se ignora cuando se juntaron los 3 en este main
+        argv[1] = (char *)malloc(2); // Asignar espacio para el flag (-c o -d)
+
+        // Determinar si se debe comprimir o descomprimir según la extensión del archivo de salida
         if (archivo_salida[strlen(archivo_salida)-4] == '.') {
             argv[1] = "-c";  // Comprimir
         } else {
             argv[1] = "-d";  // Descomprimir
         }
-        argv[2] = dir_txt;
-        argv[3] = archivo_salida;
 
-        EjecutarHuffmanFork(4, argv); // Llamar la función
+        argv[2] = dir_txt;         // Directorio de entrada
+        argv[3] = archivo_salida;  // Archivo de salida
+
+        // Llamar la función correspondiente para ejecutar Huffman usando Fork
+        EjecutarHuffmanFork(4, argv);
     }
 
     return 0;
